cdf97191fdf78922495c611aad0e6dc4397be579
fuhr-br <andersonfuhr@yahoo.com.br>
2022-03-29 11:48:44 -0300

    criando projeto

---
+++ src/main/java/com/kimetsu/KimetsuApplication.java
@@ -0,0 +1,13 @@
+package com.kimetsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimetsuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimetsuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kimetsu/KimetsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimetsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimetsuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
